11d0d3ba978fed79cf94bc3a3d11c01b1f347a59
Ralitsa <ralitsanikolova@abv.bg>
2021-10-28 14:14:08 +0300

    Modified Problem 15. Continents and Currencies.sql

--- Subqueries and Joins/Problem 15. Continents and Currencies.sql
+++ Subqueries and Joins/Problem 15. Continents and Currencies.sql
@@ -1,9 +1,16 @@
-SELECT AllRanks.ContinentCode, AllRanks.CurrencyCode, AllRanks.CurrencyUsage
+SELECT
+ AllRanks.ContinentCode,
+ AllRanks.CurrencyCode,
+ AllRanks.CurrencyUsage
 FROM
-(SELECT Countries.ContinentCode,Countries.CurrencyCode , Count(Countries.CurrencyCode) AS CurrencyUsage,
-	
+	(SELECT
+	Countries.ContinentCode,
+	Countries.CurrencyCode,
+	Count(Countries.CurrencyCode) AS CurrencyUsage,	
 	DENSE_RANK() OVER (PARTITION BY Countries.CurrencyCode ORDER BY Count(Countries.CurrencyCode)) AS CurrencyCodeRanks
 	FROM Countries
-	GROUP BY Countries.ContinentCode, Countries.CurrencyCode) AS AllRanks
-	WHERE   AllRanks.CurrencyCodeRanks = 1 and AllRanks.CurrencyUsage >1
+	GROUP BY 
+	Countries.ContinentCode,
+	Countries.CurrencyCode) AS AllRanks
+	WHERE   AllRanks.CurrencyCodeRanks = 1 and AllRanks.CurrencyUsage > 1
 	
